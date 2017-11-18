3001fc54b67b7d402fae29277fd9989367d2269f
Nick Hartjes <info@nickhartjes.nl>
2017-11-18 16:58:22 +0100

    Add Microsoft SQL Operations Studio

---
+++ Casks/microsoft-sql-operations-studio.rb
@@ -0,0 +1,10 @@
+cask 'microsoft-sql-operations-studio' do
+  version '0.23.6'
+  sha256 '25b59f32cb37298d0975489ecf0a1a7d1839ec3888ae8ae2fcaec3e62ee95814'
+
+  url 'https://go.microsoft.com/fwlink/?linkid=862647'
+  name 'Microsoft SQL Operations Studio'
+  homepage 'https://docs.microsoft.com/en-us/sql/sql-operations-studio'
+
+  app 'SQL Operations Studio.app'
+end
